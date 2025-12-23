0ca65905e6d1099146bf5731801b24da6744c468
Viduna-Abeyweera <vidunatcabeyweera@gmail.com>
2025-12-23 11:12:30 +0530

    Added the Student Class

--- model/Student.java
+++ model/Student.java
@@ -1,6 +1,66 @@
 package model;
 
-public class Student
+public class Student 
 {
-    
+
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+
+    // Paramterized Constructor
+    public Student(int id, String name, int age, String course) 
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    // Getters
+    public int getId() 
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge() 
+    {
+        return age;
+    }
+
+    public String getCourse() 
+    {
+        return course;
+    }
+
+    // Setters
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public void setAge(int age) 
+    {
+        this.age = age;
+    }
+
+    public void setCourse(String course) 
+    {
+        this.course = course;
+    }
+
+    // To display student details
+    @Override
+    public String toString() 
+    {
+        return "ID: " + id +
+               ", Name: " + name +
+               ", Age: " + age +
+               ", Course: " + course;
+    }
 }
